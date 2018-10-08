29e5ff062e2fe1a697ebb8b64edcb90a95815cdc
Cristian Gayosso Peralta <1716110078@utec-tgo.edu.mx>
2018-10-08 10:12:13 -0500

    programar en modelo boton ultimo,siguiente,primero y aterior

--- src/models/ModelAgenda.java
+++ src/models/ModelAgenda.java
@@ -41,16 +41,10 @@ public class ModelAgenda {
         this.email = email;
     }
 
-    /**
-     * Método que realiza las siguietnes acciones:
-     * 1.- Conecta con la base agenda_mvc.
-     * 2.- Consulta todo los registros de la tabla contactos.
-     * 3.- Obtiene el nombre y el email y los guarda en las variables globales
-     * nombre y email.
-     */
+
     public void conectarDB() {
         try {
-            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3309/agenda_mvc", "user_mvc", "pass_mvc.2018");
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda_mvc", "user_mvc", "pass_mvc.2018");
             st = conexion.createStatement();
             rs = st.executeQuery("SELECT * FROM contactos;");
             rs.next();
@@ -61,43 +55,21 @@ public class ModelAgenda {
         }
 
     }
-    
-    /**
-     * Método que realiza las siguiente acciones:
-     * 1.- Moverse al primer registro
-     * 2.- obtener el valor del nombre de rs y guardarlo en la variable nombre
-     * 3.- obtener el valor del email de rs y guardarlo en la variable email
-     */
+
     public void moverPrimerRegistro(){
         System.out.print("Programa accion moverPrimerRegistro");
     }
     
-    /**
-     * Método que realiza las siguiente acciones:
-     * 1.- Moverse al siguiente registro
-     * 2.- obtener el valor del nombre de rs y guardarlo en la variable nombre
-     * 3.- obtener el valor del email de rs y guardarlo en la variable email
-     */
+
     public void moverSiguienteRegistro(){
         System.out.print("Programa accion moverSiguienteRegistro");
     }
     
-    /**
-     * Método que realiza las siguiente acciones:
-     * 1.- Moverse al anterior registro
-     * 2.- obtener el valor del nombre de rs y guardarlo en la variable nombre
-     * 3.- obtener el valor del email de rs y guardarlo en la variable email
-     */
+
     public void moverAnteriorRegistro(){
         System.out.print("Programa accion moverAnteriorRegistro");
     }
     
-    /**
-     * Método que realiza las siguiente acciones:
-     * 1.- Moverse al ultimo registro
-     * 2.- obtener el valor del nombre de rs y guardarlo en la variable nombre
-     * 3.- obtener el valor del email de rs y guardarlo en la variable email
-     */
     public void moverUltimoRegistro(){
         System.out.print("Programa accion moverUltimoRegistro");
     }
